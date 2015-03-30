89941a79c821cad25e5d0c0dedf029dba6327919
anapetrovic <anapetrovic994@gmail.com>
2015-03-30 20:15:30 +0200

    Napravljena klasa KursnaLista koja implementira interfejs MenjacnicaInterfejs

---
+++ src/menjacnica/KursnaLista.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import specifikacijaInterfejsa.MenjacnicaInterfejs;
+
+public class KursnaLista implements MenjacnicaInterfejs {
+
+	public void dodajKurs(double kupovni, double prodajni, double srednji,
+			GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void obrisiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double[] pronadjiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
