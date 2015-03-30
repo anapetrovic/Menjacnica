fd54db4d79b3fd455ad426295b6580c43ffb1935
anapetrovic <anapetrovic994@gmail.com>
2015-03-30 23:00:56 +0200

    Implementirane metode klase KursnaLista, dodajKurs, obrisiKurs i pronadjiKurs

--- src/menjacnica/KursnaLista.java
+++ src/menjacnica/KursnaLista.java
@@ -1,25 +1,43 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijaInterfejsa.MenjacnicaInterfejs;
 
 public class KursnaLista implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi= new LinkedList<Kurs>();
 
 	public void dodajKurs(double kupovni, double prodajni, double srednji,
 			GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		Kurs noviKurs = new Kurs(kupovni, srednji, prodajni, datum);
+		kursevi.add(noviKurs);
 	}
 
 	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < kursevi.size(); i++) {
+			if(kursevi.get(i).getDatum().equals(datum))
+				kursevi.remove(kursevi.get(i));
+			
+		}
 
 	}
 
 	public double[] pronadjiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+		double[] kurseviNaDan = new double[3];
+		for (int i = 0; i < kursevi.size(); i++) {
+			if(kursevi.get(i).getDatum().equals(datum)){
+				kurseviNaDan[0]=kursevi.get(i).getKupovni();
+				kurseviNaDan[1]=kursevi.get(i).getProdajni();
+				kurseviNaDan[2]=kursevi.get(i).getSrednji();
+				break;
+				
+			}
+				
+			
+		}
+		return kurseviNaDan;
 	}
 
 }
